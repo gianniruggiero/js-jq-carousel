41e1c3352c6e2a0d4c3e2809779e3840df597853
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-02 15:59:14 +0200

    impostato: scorre immagini carousel al click sulle frecce

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+  // *** NEXT IMG ***
+  // al click su freccia destra mostra immagine successiva
+  $(".next").click(imgNext);
+
+  // *** PREV IMG ***
+  // al click su freccia sinistra mostra immagine successiva
+  $(".prev").click(imgPrev)
+
+  // funzione che mostra l'immagine successiva del carousel;
+  // se l'immagine al momento del click è l'ultima del carousel allora mostra la prima.
+  function imgNext() {
+    // individua l'immagine visualizzata al momento del click
+    var imgAttiva = $("img.active");
+    // nasconde l'immagine attualmente visualizzata
+    imgAttiva.removeClass("active");
+    // verifica se l'immagine attuale è l'ultima del carousel
+    if (imgAttiva.hasClass("last")) {
+      // come immagine NEXT imposta la prima immagine del carousel
+      var imgNext = $("img.first");
+    } else {
+      // come immagine NEXT imposta limmagine successiva nel carousel
+      var imgNext = imgAttiva.next();
+    }
+    // fa comparire l'immagine definita come successiva dal controllo
+    imgNext.addClass("active");
+  }
+
+  // funzione che mostra l'immagine precedente del carousel;
+  // se l'immagine al momento del click è la prima del carousel allora mostra l'ultima.
+  function imgPrev() {
+    // individua l'immagine visualizzata al momento del click
+    var imgAttiva = $("img.active");
+    // nasconde l'immagine attualmente visualizzata
+    imgAttiva.removeClass("active");
+    // verifica se l'immagine attuale è la prima del carousel
+    if (imgAttiva.hasClass("first")) {
+      // è la 1^ img del carousel: come immagine PREV imposta l'ultima immagine del carousel
+      var imgPrev = $("img.last");
+    } else {
+      // come immagine PREV imposta l'immagine precedente nel carousel
+      var imgPrev = imgAttiva.prev();
+    }
+    // fa comparire l'immagine definita come precedente dal controllo
+    imgPrev.addClass("active");
+  }
+
+
+});
